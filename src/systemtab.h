890f6f24bdb15914eeb9b98c830d294552ae001c
Margherita Rufi <margherita.rufi@epfl.ch>
2023-12-04 18:40:03 +0100

    SystemTab basics completed

--- src/systemtab.h
+++ src/systemtab.h
@@ -15,78 +15,9 @@ namespace Ui {
 class SystemTab;
 }
 
+class SystemTabView;
 class SystemBlock;
-
-class Arrow {
-
-public:
-  // Arrow(SystemBlock *startItem, SystemBlock *endItem, const QColor &color,
-  // int distanceFromBus); void updateArrowLine(QGraphicsItem* startItem,
-  // QGraphicsItem* endItem, qreal baseDistanceFromBus);
-  QGraphicsLineItem &getArrowLine() { return m_arrowLine; }
-  QPen &getPen() { return m_pen; }
-  // QPainter& getPainter() {return m_painter;}
-  QGraphicsPolygonItem &getArrowHeadItemUp() { return m_arrowHeadItemUp; }
-  QGraphicsPolygonItem &getArrowHeadItemDown() { return m_arrowHeadItemDown; }
-
-protected:
-private:
-  QGraphicsLineItem m_arrowLine; // = nullptr; //it was *arrowLine
-  // QPainter m_painter;
-  QPen m_pen;
-
-  /*QPointF arrowP1Up;
-  QPointF arrowP2Up;
-  QPointF arrowP1Down;
-  QPointF arrowP2Down;
-
-  QPolygonF arrowHeadUp, arrowHeadDown;*/
-
-  QGraphicsPolygonItem m_arrowHeadItemUp;
-  QGraphicsPolygonItem m_arrowHeadItemDown;
-};
-
-enum BlockType { CPU, MEMORY, BUS, PERIPHERAL };
-
-class SystemBlock : public QGraphicsRectItem {
-  // Q_OBJECT
-
-public:
-  SystemBlock(BlockType type, std::string name, qreal x, qreal y, qreal width,
-              qreal height, QGraphicsItem *parent = nullptr);
-  QString getQName() { return QString::fromStdString(m_name); }
-  BlockType getBlockType() { return m_type; }
-  void setName(std::string name) { m_name = name; }
-  void updateLabel(const QString &newName);
-  Arrow &getArrow() { return m_arrow; }
-
-  // deconstructor?
-
-protected:
-private:
-  BlockType m_type;
-  std::string m_name;
-  QGraphicsTextItem *m_label; // perché pointer?
-  QPointF cpuLabelPosition;   //????
-  Arrow m_arrow; // = nullptr; //pointer to avoid initializing it when creating
-                 // a block (don't know endItem yet)
-  QGraphicsPolygonItem m_arrowHeadItemUp;
-};
-
-class SystemTabView : public QGraphicsView {
-  Q_OBJECT
-
-public:
-  SystemTabView(QWidget *parent);
-  // deconstructor?
-
-protected:
-  void wheelEvent(QWheelEvent *event) override;
-
-private:
-  void zoomIn();
-  void zoomOut();
-};
+class Arrow;
 
 class SystemTab : public RipesTab {
   Q_OBJECT
@@ -98,12 +29,13 @@ public:
 private:
   Ui::SystemTab *m_ui = nullptr;
 
-  int baseBlockDimension = 100;
+  int baseBlockDimension = 150;
   int baseBusHeight = 45;
   int baseSpaceBetweenBlocks = 20;
   int baseDistanceFromBus = 50;
   int startSceneX = 0;
   int startSceneY = 0;
+  int arrowThickness = 5;
 
   QGraphicsScene *scene;
   SystemTabView *view;
@@ -123,11 +55,63 @@ private:
   void addNewPeripherals(std::set<IOBase *> &activePeripherals);
   void connectItemsWithArrow(SystemBlock *startItem, SystemBlock *endItem,
                              const QColor &color);
+  int busLength(int nbActivePeripherals);
 
 private slots:
   void onListItemClicked(QListWidgetItem *item);
-  // void doSomething();
-  // void doSomething2();
+};
+
+class SystemTabView : public QGraphicsView {
+  Q_OBJECT
+
+public:
+  SystemTabView(QWidget *parent);
+
+protected:
+  void wheelEvent(QWheelEvent *event) override;
+
+private:
+  void zoomIn();
+  void zoomOut();
+};
+
+class Arrow {
+
+public:
+  QGraphicsLineItem &getArrowLine() { return m_arrowLine; }
+  QPen &getPen() { return m_pen; }
+  QGraphicsPolygonItem &getArrowHeadItemUp() { return m_arrowHeadItemUp; }
+  QGraphicsPolygonItem &getArrowHeadItemDown() { return m_arrowHeadItemDown; }
+
+private:
+  QGraphicsLineItem m_arrowLine; // = nullptr; //it was *arrowLine
+  QPen m_pen;
+  QGraphicsPolygonItem m_arrowHeadItemUp;
+  QGraphicsPolygonItem m_arrowHeadItemDown;
+};
+
+enum BlockType { CPU, MEMORY, BUS, PERIPHERAL };
+
+class SystemBlock : public QGraphicsRectItem {
+  // Q_OBJECT
+
+public:
+  SystemBlock(BlockType type, std::string name, qreal x, qreal y, qreal width,
+              qreal height, QGraphicsItem *parent = nullptr);
+  QString getQName() { return QString::fromStdString(m_name); }
+  BlockType getBlockType() { return m_type; }
+  void setName(std::string name) { m_name = name; }
+  void updateLabel(const QString &newName);
+  Arrow &getArrow() { return m_arrow; }
+  QGraphicsTextItem &getLabel() { return m_label; }
+
+private:
+  BlockType m_type;
+  std::string m_name;
+  QGraphicsTextItem m_label; // perché pointer?
+  QPointF labelPosition;
+  Arrow m_arrow; // = nullptr;
+  QGraphicsPolygonItem m_arrowHeadItemUp;
 };
 
 } // namespace Ripes
